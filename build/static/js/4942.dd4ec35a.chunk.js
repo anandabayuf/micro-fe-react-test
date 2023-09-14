0f1116c434ced4e36ff160ba2364fcacdeaf71f4
Ananda Bayu <anandabayu090@gmail.com>
2023-09-14 10:52:18 +0700

    add build

---
+++ build/static/js/4942.dd4ec35a.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkproject_bni_dashboard=self.webpackChunkproject_bni_dashboard||[]).push([[4942],{4942:function(e,n,r){function t(e,n,r){return n in e?Object.defineProperty(e,n,{value:r,enumerable:!0,configurable:!0,writable:!0}):e[n]=r,e}r.d(n,{Z:function(){return t}})}}]);
